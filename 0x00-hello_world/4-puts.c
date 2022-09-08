8cfc2fbe763fe6e13a4459807dea8792eccc5fa4
dybole68 <paulitosan68@gmail.com>
2022-09-08 15:59:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdioh>
+/**
+ * main-printing
+ *
+ * Return: Always 0(successful)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
